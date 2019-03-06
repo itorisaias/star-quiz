597a0b8c23e0c9cf571dfe18b396e4ee81edf55c
Itor Isaias <itor.isaias@gmail.com>
2019-03-06 13:19:46 -0300

    Install plugin vuetify

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
+import 'vuetify/src/stylus/app.styl'
+import pt from 'vuetify/es5/locale/pt'
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#ee44aa',
+    secondary: '#424242',
+    accent: '#82B1FF',
+    error: '#FF5252',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FFC107'
+  },
+  options: {
+    customProperties: true
+  },
+  iconfont: 'md',
+  lang: {
+    locales: { pt },
+    current: 'pt'
+  }
+})
