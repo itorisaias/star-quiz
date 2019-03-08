5a46a5cd1f61b129c4f1f7387ddf8d8c6aac00bb
Itor Isaias <itor.isaias@gmail.com>
2019-03-08 19:21:28 -0300

    update title and link to repository

--- src/components/core/Toolbar.vue
+++ src/components/core/Toolbar.vue
@@ -1,11 +1,22 @@
 <template>
   <v-toolbar app dense dark>
-    <v-toolbar-title>Title</v-toolbar-title>
+    <v-toolbar-title>{{ title }}</v-toolbar-title>
 
     <v-spacer></v-spacer>
 
     <v-btn icon>
-      <v-icon>more_vert</v-icon>
+      <a class="white--text" :href="urlRepository" target="blank">
+        <v-icon>cloud_download</v-icon>
+      </a>
     </v-btn>
   </v-toolbar>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    urlRepository: 'https://github.com/itorisaias/star-quiz',
+    title: 'Star Quiz - Invillia'
+  })
+}
+</script>
