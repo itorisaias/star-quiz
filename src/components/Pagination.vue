e09eb7284cc4880ed59e60c279f533d30df0b50b
Itor Isaias <itor.isaias@gmail.com>
2019-03-09 04:28:28 -0300

    externalization of the paging component for general use

---
+++ src/components/Pagination.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="text-xs-center">
+    <v-pagination
+      dark
+      v-model="page"
+      :length="length"
+      :total-visible="totalVisible"
+      color="yellow"
+    ></v-pagination>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      page: 1
+    }
+  },
+  props: {
+    length: {
+      type: Number,
+      required: true
+    },
+    totalVisible: {
+      type: Number,
+      default: 7
+    }
+  },
+  watch: {
+    page (value) {
+      this.$emit('paginate', value)
+    }
+  }
+}
+</script>
--- src/components/quiz/Pagination.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>Paginação</h1>
-</template>
