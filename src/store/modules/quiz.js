1043bc629827d31e3ce7a5c50d536b86ca1307f0
Itor Isaias <itor.isaias@gmail.com>
2019-03-10 11:19:31 -0300

    create store for quiz

--- src/store/modules/quiz.js
+++ src/store/modules/quiz.js
@@ -1,16 +1,30 @@
 const state = {
-  answers: [],
-  time: null,
-  points: 0
+  time: 0,
+  timeId: null
 }
 
 const mutations = {
+  setTime: (state, payload) => (state.time = payload),
+  setTimeId: (state, payload) => (state.timeId = payload)
 }
 
 const actions = {
+  startQuiz ({ commit }) {
+    // TODO: logica do boot do quiz
+  },
+  bootTime ({ commit }, { time, timeId }) {
+    commit('setTime', time)
+    commit('setTimeId', timeId)
+  },
+  finishQuiz ({ state, commit }) {
+    clearInterval(state.timeId)
+    commit('setTimeId', null)
+    // TODO: logica de finalização
+  }
 }
 
 const getters = {
+  time: state => state.time
 }
 
 export default {
