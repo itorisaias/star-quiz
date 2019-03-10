6dd966b2b1866ff346ecb2402feb5f65e9124864
Itor Isaias <itor.isaias@gmail.com>
2019-03-10 11:12:56 -0300

    create modal finish game

---
+++ src/components/quiz/ModalFinish.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-dialog persistent :value="!time">
+    <v-card>
+      <v-card-title>
+        Fim de Jogo
+      </v-card-title>
+      <v-card-text>
+        <h1>Fim do jogo XAU</h1>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn to="/ranking">OK</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters('quiz', ['time'])
+  }
+}
+</script>
