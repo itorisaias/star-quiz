75420cc778449a18d29407b568bd65721352d387
Itor Isaias <itor.isaias@gmail.com>
2019-03-11 02:30:34 -0300

    separating the screen into components

---
+++ src/components/ranking/Title.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-card-title>
+    <v-spacer></v-spacer>
+    <span class="display-3">Classificação de Jogadas</span>
+    <v-spacer></v-spacer>
+  </v-card-title>
+</template>
--- src/views/Ranking.vue
+++ src/views/Ranking.vue
@@ -1,67 +1,21 @@
 <template>
   <v-card>
-    <v-card-title>
-      Ranking
-      <v-spacer></v-spacer>
-      <v-text-field
-        v-model="search"
-        append-icon="search"
-        label="Search"
-        single-line
-        hide-details
-      ></v-text-field>
-    </v-card-title>
 
-    <v-data-table
-      :headers="headers"
-      :items="plays"
-      :pagination.sync="pagination"
-      class="elevation-1"
-    >
-      <template v-slot:items="props">
-        <td>{{ props.item.username }}</td>
-        <td>{{ props.item.email }}</td>
-        <td>{{ props.item.totalPoints }}</td>
-        <td>{{ props.item.countOfQuestion }}</td>
-      </template>
-    </v-data-table>
+    <ranking-title />
 
-    <v-card-actions>
-      <v-btn v-if="showBtnPlayning" to="/playing">JOGAR NOVAMENTE</v-btn>
-      <v-btn to="/">INICIO</v-btn>
-    </v-card-actions>
+    <ranking-table />
+
+    <ranking-actions />
 
   </v-card>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-
 export default {
-  data: () => ({
-    showBtnPlayning: false,
-    search: null,
-    pagination: {
-      sortBy: 'totalPoints',
-      descending: true
-    },
-    headers: [
-      { text: 'Nome', value: 'username' },
-      { text: 'Email', value: 'email' },
-      { text: 'Pontos', value: 'totalPoints' },
-      { text: 'Qtd. Respostas', value: 'countOfQuestion' }
-      // align: 'left', sortable: false,
-    ]
-  }),
-  computed: {
-    ...mapGetters('ranking', ['plays'])
-  },
-  created () {
-    const { finished } = this.$route.query
-
-    if (finished !== undefined) {
-      this.showBtnPlayning = true
-    }
+  components: {
+    RankingActions: () => import('@/components/ranking/Actions'),
+    RankingTitle: () => import('@/components/ranking/Title'),
+    RankingTable: () => import('@/components/ranking/Table')
   }
 }
 </script>
