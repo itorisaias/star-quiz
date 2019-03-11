81f501d5d08f674576f9478d6c884d056204a58e
Itor Isaias <itor.isaias@gmail.com>
2019-03-11 08:30:53 -0300

    test home pase

--- tests/unit/views/Home.spec.js
+++ tests/unit/views/Home.spec.js
@@ -1,4 +1,21 @@
-describe('Name of the group', () => {
-  it('should ', () => {
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import { shallowMount } from '@vue/test-utils'
+import Home from '@/views/Home.vue'
+import '@/utils/plugins/base'
+
+describe('Home.vue', () => {
+  Vue.use(Vuetify)
+
+  it('renders ', () => {
+    const fn = jest.fn()
+
+    shallowMount(Home, {
+      methods: {
+        getUrlImgRandon: fn
+      }
+    })
+
+    expect(fn).toBeCalled()
   })
 })
