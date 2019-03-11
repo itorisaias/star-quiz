748746b79dc11657aa9179cdcd6cf5bf6fa85177
Itor Isaias <itor.isaias@gmail.com>
2019-03-11 04:27:10 -0300

    improved viewing

--- src/components/quiz/ModalDetail.vue
+++ src/components/quiz/ModalDetail.vue
@@ -1,8 +1,8 @@
 <template>
-  <v-dialog v-if="person" persistent :value="person">
+  <v-dialog v-if="person" persistent :value="person" max-width="900px">
     <v-card>
-      <v-card-title>
-        Detalhes sobre personagem
+      <v-card-title >
+        <span class="display-2">Detalhes sobre personagem</span>
         <v-spacer></v-spacer>
         <v-btn icon @click="closeDetail">
           <v-icon>close</v-icon>
@@ -11,44 +11,69 @@
       <v-card-text>
         <v-layout row wrap>
           <v-flex xs6>
-            <v-img
-              :src="person.urlImg"
-              class="my-3"
-              contain
-              height="200"
-            />
+            <v-img :src="person.urlImg" class="my-3" contain height="200" />
           </v-flex>
           <v-flex xs6>
-            <ul>
-              <li>
-                <strong>Specie: </strong>
-                {{ person.species.map((specie) => specie.name).join(', ') || 'n/a' }}
-              </li>
-              <li>
-                <strong>Height: </strong>
-                {{ person.height }}
-              </li>
-              <li>
-                <strong>Hair: </strong>
-                {{ person.hair_color }}
-              </li>
-              <li>
-                <strong>Planet: </strong>
-                {{ person.homeworld }}
-              </li>
-            </ul>
+            <v-list>
+              <v-divider></v-divider>
+              <v-list-tile>
+                <v-list-tile-content>
+                  <v-list-tile-title>
+                    <strong>Espécie: </strong>
+                    {{ person.species.map((specie) => specie.name).join(', ') || 'n/a' }}
+                  </v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+              <v-divider></v-divider>
+              <v-list-tile>
+                <v-list-tile-content>
+                  <v-list-tile-title>
+                    <strong>Altura: </strong>
+                    {{ person.height }}
+                  </v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+              <v-divider></v-divider>
+              <v-list-tile>
+                <v-list-tile-content>
+                  <v-list-tile-title>
+                    <strong>Cor de cabelo: </strong>
+                    {{ person.hair_color }}
+                  </v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+              <v-divider></v-divider>
+              <v-list-tile>
+                <v-list-tile-content>
+                  <v-list-tile-title>
+                    <strong>Planeta: </strong>
+                    {{ person.homeworld }}
+                  </v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+              <v-divider></v-divider>
+            </v-list>
           </v-flex>
           <v-flex xs12>
-            <p>
-              <strong>filmes: </strong>
-              {{ person.films.map((film) => film.title).join(', ') || 'n/a' }}
-            </p>
-          </v-flex>
-          <v-flex xs12>
-            <p>
-              <strong>veiculos: </strong>
-              {{ person.vehicles.map((vehicle) => vehicle.name).join(', ') || 'n/a' }}
-            </p>
+            <v-list>
+              <v-list-tile>
+                <v-list-tile-content>
+                  <v-list-tile-title>
+                    <strong>Filmes: </strong>
+                    {{ person.films.map((film) => film.title).join(', ') || 'n/a' }}
+                  </v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+              <v-divider></v-divider>
+              <v-list-tile>
+                <v-list-tile-content>
+                  <v-list-tile-title>
+                    <strong>Veículos: </strong>
+                    {{ person.vehicles.map((vehicle) => vehicle.name).join(', ') || 'n/a' }}
+                  </v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+            </v-list>
           </v-flex>
         </v-layout>
       </v-card-text>
